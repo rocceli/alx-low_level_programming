8bc4b96547c59f0a21c542fd6c1b6e3c85ba20d7
Elijah <ngugielijah08@gmail.com>
2023-02-17 12:50:06 -0800

    Hello world part 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 
-int main()
+int main(void)
 {
-	char str[]="Programming is like building a multilingual puzzle";
+	char str[] = ""Programming is like building a multilingual puzzle";
+
 	puts(str);
 	return 0;
 }
