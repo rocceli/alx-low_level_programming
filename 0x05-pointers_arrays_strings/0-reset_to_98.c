1a994aee8a634074a67517fb75dcadc4fc5130ae
Elijah <ngugielijah08@gmail.com>
2023-02-28 14:07:52 -0800

    update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 
 /**
  * reset_to_98 - check code
- *
+ *@n:argument
  * Return: Always 0
  */
 void reset_to_98(int *n)
--- 0x05-pointers_arrays_strings/4-main.c
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -1,4 +1,4 @@
-#include "main."
+#include "main.h"
 
 /**
  * main - check code
