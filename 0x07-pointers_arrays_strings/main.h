d51b5e9e73f87a558987785174e8b027a0a3edb3
Elijah <ngugielijah08@gmail.com>
2023-03-07 15:23:19 -0800

    Even more pointers and arrays in c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 
+int _putchar(char);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 char *_strstr(char *haystack, char *needle);
